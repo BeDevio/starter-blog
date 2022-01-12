38f3915a7be1fd57b357a6274310c3fd4d2318e9
BeDev <33584858+BeDevio@users.noreply.github.com>
2022-01-12 16:38:41 -0500

    Made some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `BeDev`,
+      summary: `Makes youtube videos about small but useful developer tips`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `The website of Bedev`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `anything`,
     },
   },
   plugins: [
